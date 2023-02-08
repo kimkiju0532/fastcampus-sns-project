713f48bd2bc8816e734b6eddf08ec2f148d92f32
KIMK <kimk0532@gmail.com>
2023-02-08 23:21:47 +0900

    Feat: 포스트 삭제 기능 추가

--- src/main/java/com/fastcampus/snsproject/controller/PostController.java
+++ src/main/java/com/fastcampus/snsproject/controller/PostController.java
@@ -6,8 +6,11 @@ import com.fastcampus.snsproject.controller.response.Response;
 import com.fastcampus.snsproject.model.Post;
 import com.fastcampus.snsproject.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/api/v1/posts")
@@ -29,9 +32,19 @@ public class PostController {
     }
 
     @DeleteMapping("/{postId}")
-    public  Response<Void> delete(@PathVariable Integer postId, Authentication authentication) {
+    public Response<Void> delete(@PathVariable Integer postId, Authentication authentication) {
         postService.delete(authentication.getName(), postId);
         return Response.success();
+    }
+
+    @GetMapping
+    public Response<Page<PostResponse>> list(Pageable pageable, Authentication authentication) {
+        return Response.success(postService.list(pageable).map(PostResponse::fromPost));
+    }
+
+    @GetMapping("/my")
+    public Response<Page<PostResponse>> my(Pageable pageable, Authentication authentication) {
+        return Response.success(postService.my(authentication.getName(), pageable).map(PostResponse::fromPost));
     }
 
 }
